01fd6a2e2de6e217732568a57ec42a65e12489d9
Nishant Dubey <nishantdubey711@gmail.com>
2026-09-17 09:25:05 +0530

    Add: minimum-number-of-swaps-to-make-the-string-balanced - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-swaps-to-make-the-string-balanced/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        stackSize = 0
+        for c in s:
+            if c == '[':
+                stackSize += 1
+            elif stackSize > 0:
+                stackSize -= 1
+        return (stackSize + 1) // 2
+        
